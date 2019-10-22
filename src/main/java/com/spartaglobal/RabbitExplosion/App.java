774a9a80a8d99dacccf1bfdd17b36e952e660b25
NGeorgiev <NGeorgiev@spartaglobal.com>
2019-10-22 15:19:48 +0100

    initial commit

---
+++ src/main/java/com/spartaglobal/RabbitExplosion/App.java
@@ -0,0 +1,13 @@
+package com.spartaglobal.RabbitExplosion;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
